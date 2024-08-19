8110c7f122fd5b8379e90c6d310af83f90ef20b9
lucid-techwrld <davisredtiger@gmail.com>
2024-08-19 20:43:21 +0100

    textGenerator

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+
+
+function generateText() {
+  let text = document.getElementById('textToGenerate').value;
+  const num = document.getElementById('numToGenerate').value;
+  let output = ""; // Reset output each time the function runs
+
+  for (let i = 0; i < num; i++) {
+    output += text + "\n";
+  }
+
+  document.getElementById('result').textContent = output; // Use innerHTML to interpret the <br> tags
+}
+
+document.getElementById("copyIcon").addEventListener("click", function() {
+    // Select the text field
+    var text = document.getElementById("result");
+
+    // Select the text inside the input field
+    text.select();
+    text.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the input field
+    navigator.clipboard.writeText(text.value).then(function() {
+        alert("Text copied to clipboard!");
+    }).catch(function(error) {
+        alert("Failed to copy text: " + error);
+    });
+});
